65c1de65cbd7e25df304e4e9ffddde10fa53761a
SebastianRivasUG <157664620+SebastianRivasUG@users.noreply.github.com>
2024-02-21 00:52:59 +0000

    Pruebas testnode FALTA ACABAR esa prueba (no es muy relevante)

---
+++ Proyecto 1 INFO3/tests/testnode.java
@@ -0,0 +1,40 @@
+package tests;
+
+import transactions.Node;
+import exceptions.NodeInvalidException;
+
+import java.io.*;
+
+public class testnode{
+
+  private String type;
+  private String date;
+  private double amount;
+  private String key;
+
+  BufferedReader tec = new BufferedReader(new InputStreamReader(System.in));
+
+  public void pedirDatos() throws IOException {
+    System.out.print("Ingrese tipo de transacción: ");
+    this.type = tec.readLine();
+    System.out.print("\n Ingrese tipo de transacción: ");
+    this.date = tec.readLine();
+    System.out.print("\n Ingrese tipo de transacción: ");
+    String amountS = tec.readLine();
+    this.amount = Double.parseDouble(amountS);
+    System.out.print("\n String inicial (PRUEBA) : DE2023-08-12 21:44:041000.00");
+    this.key = "DE2023-08-12 21:44:041000.00";
+
+    Node test = new Node(this.type, this.date, this.amount, this.key);
+
+    String newKey = test.generateNewKey(this.key);
+  
+    System.out.println("clave nueva gen: " + newKey);
+  }
+  
+  public static void main(String[] prueba) throws Exception{
+    testnode testN = new testnode();
+    testN.pedirDatos();
+  }
+  
+}
